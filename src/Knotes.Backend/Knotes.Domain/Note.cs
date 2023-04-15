f0c03d1d7a790197d85ab70f6604436565a7e9c2
Dmitry Korobeyshchikov <korobeyshchikov.d@mail.ru>
2023-04-15 23:02:44 +0300

    Built the architecture of the future project

---
+++ src/Knotes.Backend/Knotes.Domain/Note.cs
@@ -0,0 +1,17 @@
+﻿namespace Knotes.Domain
+{
+    public class Note
+    {
+        public Guid UserId { get; set; }
+        
+        public Guid Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Details { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public DateTime? EditDate { get; set;}
+    }
+}
